024bae4960f6a24584153dbb122bbb923d7e8ccf
Muhammad Noviyanto <muhammad.noviyanto.ltdev@gmail.com>
2019-08-29 15:24:47 +0800

    add all activity

---
+++ app/src/main/java/com/example/lenovo/xnote/nota_belanja.java
@@ -0,0 +1,11 @@
+package com.example.lenovo.xnote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class nota_belanja extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nota_belanja);
+    }
+}
